72261243537570292e7a85ea877bc1cd0d59e3f5
Isabela Palmerio <belpalmerio@hotmail.com>
2024-10-17 18:56:08 -0300

    Font

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,13 +4,13 @@ import logo from "../../assets/icons/bell-lets-talk.png";
 import "./Header.scss";
 
 function Header() {
-  return (
-    <header className="app-header">
-      <NavLink className="app-header__logolink" to="/">
-        <img src={logo} />
-      </NavLink>
-    </header>
-  );
+    return (
+        <header className="app-header">
+            <NavLink className="app-header__logolink" to="/">
+                <img src={logo} />
+            </NavLink>
+        </header>
+    );
 }
 
 export default Header;
