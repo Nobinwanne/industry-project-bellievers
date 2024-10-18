a826e02ce7a84f12bbbaf1446e541ad825da44d7
Nobinwanne <ndidiobinwanne@gmail.com>
2024-10-18 16:01:02 -0600

    add moments router

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
+import router from "./routes/momentsRouter.js";
 
 const app = express();
 
 const PORT = 8080;
 
 app.use(cors());
+app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/moments", router);
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to my server");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
