62e1ca5188774166692efe31f5b848acaa3d2fdd
Isabela Palmerio <belpalmerio@hotmail.com>
2024-10-18 14:47:09 -0300

    Stats

--- client/src/pages/TimeoutPage/TimeoutPage.jsx
+++ client/src/pages/TimeoutPage/TimeoutPage.jsx
@@ -9,22 +9,30 @@ function TimeoutPage() {
         document.title = "Mindful Moments";
     }, []);
 
-    //still to do here....
-    //fix styling
-    //add correct content
-
     return (
         <>
             <article className="text">
                 <section className="text__container">
-                    <h1 className="text__body">Thinking of you. Love, Mom</h1>
-                    <p className="text__body">Gift goes here if there is one</p>
+                    <h1 className="text__body">
+                        Thinking of you. Let's Talk soon. Love, Mom
+                    </h1>
+                    <p className="text__body">Treat yourself! $5 for Coffee</p>
                     <p className="text__body">
-                        "Everyday ............." - Stats Canada idk lorem ipsum
-                        fsldafsfdfffffffwfesfesafsae sfsdfsdfsdfsfsdfssb asfba
-                        fas kfsfbsd
+                        "33% of employed Canadians are burned out.* In some
+                        professions, however, it is much higher (e.g., 66% of
+                        nurses, and 61% of the mental health workforce)"
                     </p>
-                    <p className="text__body">add some resource here</p>
+                    <p className="text__body">
+                        15% of Canadians have accessed mental health services in
+                        the last year, an additional 6% believe they should but
+                        are not."
+                    </p>
+                    <p className="text__body">
+                        1 in 8 Canadians have been experiencing high levels of
+                        anxiety. Those indicating feeling high anxiety was 7%
+                        just before the pandemic and 11% now. 1 in 4 Canadians
+                        will receive a lifetime diagnosis of anxiety."
+                    </p>
                 </section>
             </article>
             <article className="static">
